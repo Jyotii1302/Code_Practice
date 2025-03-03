dff8ab57fc4fa6308bdc362153a5d9e3bb20822e
Jyoti choudhary <jyotiichoudhary1310@gmail.com>
2025-03-03 13:41:12 +0530

    Create StringDuplicateRemoval.cpp

---
+++ StringDuplicateRemoval.cpp
@@ -0,0 +1,13 @@
+    string removeDuplicates(string &s) {
+        bool freq[256]={false};
+        string result;
+        
+        for (int i = 0; i < s.length(); i++) {
+        if (!freq[s[i]]) {
+            freq[s[i]] = true;
+            result += s[i];
+        }
+    }
+    return result;
+}
+};
